c337a6465391d49b3286dc9003027cb30af28b8f
Travis Vander Hoop <vanderhoop@me.com>
2013-12-08 22:39:04 -0500

    contact nav now conjures prepopulated email to  sayhello@yourfriendtravis.com

--- javascripts/abstractions.js
+++ javascripts/abstractions.js
@@ -9,7 +9,8 @@ function goToWork(){
 
 function goToContact(){
   console.log("goToContact function works");
-  window.location.hash = "contact";
+  // conjures email with prepopulated fields
+  window.location = "mailto:sayhello@yourfriendtravis.com?Subject=Greetings!&body=Hello%20Travis%2C%0AMy%20name%20is%20________.%20I%27d%20like%20to%20get%20in%20touch!%20I%20can%20best%20be%20reached%20by%20(phone%2Femail)%20at%3A%20______________%20.%20%0A%0ATalk%20soon%2C%20%0A(Your%20name)";
 }
 
 function goToHome(){
