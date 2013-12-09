1bdec7a2eae39c6ce30faf14ba2dec18083fac8e
Travis Vander Hoop <vanderhoop@me.com>
2013-12-08 21:28:21 -0500

    added event listeners to the nav list items

---
+++ javascripts/abstractions.js
@@ -0,0 +1,7 @@
+function goToWork(){
+  console.log("goToWork function works");
+}
+
+function goToContact(){
+  console.log("goToContact function works");
+}
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,5 @@
 $( document ).ready(function() {
   console.log("jquery and main.js are linked");
+  $('#work-nav').on("click", goToWork);
+  $('#contact-nav').on("click", goToContact);
 });
