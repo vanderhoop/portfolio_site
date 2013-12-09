e4ed9a73016a266896c484cb639f0d3a5f9f86a3
Travis Vander Hoop <vanderhoop@me.com>
2013-12-08 22:07:05 -0500

    removed pesky hash symbol from fragment identifier  when navigating back to Home from Work || Contact

--- javascripts/abstractions.js
+++ javascripts/abstractions.js
@@ -1,7 +1,19 @@
+function removeHash () {
+    history.pushState("", document.title, window.location.pathname + window.location.search);
+}
+
 function goToWork(){
   console.log("goToWork function works");
+  window.location.hash = "work";
 }
 
 function goToContact(){
   console.log("goToContact function works");
+  window.location.hash = "contact";
+}
+
+function goToHome(){
+  console.log("goToHome function works");
+  // removes pesky hash symbol when navigating back to Home from Work || Contact
+  removeHash();
 }
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-  console.log("jquery and main.js are linked");
   $('#work-nav').on("click", goToWork);
   $('#contact-nav').on("click", goToContact);
+  $('#home-nav').on("click", goToHome);
 });
