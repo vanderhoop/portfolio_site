9ee0dc6af82972817d4e333de79f0a127899bb60
Travis Vander Hoop <vanderhoop@me.com>
2013-12-10 13:04:21 -0500

    deleted old comments and unused, no-longer-relevant code.

--- javascripts/abstractions.js
+++ javascripts/abstractions.js
@@ -7,9 +7,6 @@ function removeHash () {
 function populateWorkView(){
   console.log("populateWorkView firing");
   $('#title-div').fadeIn();
-  // $('.project-photo').fadeIn();
-  // $('.project-text').fadeIn();
-  // $('.work').fadeIn();
   $('.work').animate({ opacity: 1 }, 500);
 }
 
@@ -44,19 +41,13 @@ function goToContact(){
     title.html('Contact');
     title.css('width', "3em");
     $('.' + hash).animate({ opacity:0 }, 300).queue(populateContactView);
-
-    // $('.'+hash).fadeTo("slow", 0).queue(populateContactView);
-    // $('.'+ hash).fadeOut(300).queue(populateContactView);
     $('.work, .home').css("display", "none");
   }
   window.location.hash = "contact";
-  // conjures email with prepopulated fields
-  // window.location = "mailto:sayhello@yourfriendtravis.com?Subject=Greetings!&body=Hello%20Travis%2C%0AMy%20name%20is%20________.%20I%27d%20like%20to%20get%20in%20touch!%20I%20can%20best%20be%20reached%20by%20(phone%2Femail)%20at%3A%20______________%20.%20%0A%0ATalk%20soon%2C%20%0A(Your%20name)";
 }
 
 function goToHome(){
-  // removes pesky hash symbol when navigating back to Home from Work || Contact
+  // removes pesky #work || #contact hash when navigating back to Home from Work || Contact
   removeHash();
   window.top.location=window.top.location;
-  // location.reload(false);
 }
