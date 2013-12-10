2406d7a72196beb2c300f986361a9263b2323f1d
Travis Vander Hoop <vanderhoop@me.com>
2013-12-10 12:55:06 -0500

    changed fadeOut()'s to the jQuery .animate() method so as to only alter css and not add inline styles

--- javascripts/abstractions.js
+++ javascripts/abstractions.js
@@ -5,39 +5,49 @@ function removeHash () {
 }
 
 function populateWorkView(){
-  console.log("popWorkView firing");
+  console.log("populateWorkView firing");
   $('#title-div').fadeIn();
   // $('.project-photo').fadeIn();
   // $('.project-text').fadeIn();
-  $('.work').fadeIn();
+  // $('.work').fadeIn();
+  $('.work').animate({ opacity: 1 }, 500);
 }
 
 function goToWork(){
+
   console.log("goToWorkFiring");
+  $('.work').removeAttr("display").removeAttr("style");
   debugger
   var hash = window.location.hash.substring(1);
   if (hash != "work") {
+    // changes title of page and alters length of title underscore
     title.html('Work');
     title.css('width', "2em");
-    $('.'+hash).fadeOut(300).queue(populateWorkView);
-    $('.contact, .home').attr("display", "none");
+    // sets opacity of all divs of the current view class to 0
+    // populateWorkView is fired after animation complete
+    $('.' + hash).animate({opacity:0}, 500).queue(populateWorkView);
+    $('.contact, .home').css("display", "none");
   }
   window.location.hash = "work";
 }
 
 function populateContactView(){
   $('#title-div').fadeIn();
-  $('.contact').fadeIn();
   // all contact info
+  $('.contact').animate({ opacity: 1 }, 500);
 }
 
 function goToContact(){
+  $('.contact').removeAttr("display").removeAttr("style");
   var hash = window.location.hash.substring(1);
   if (hash != "contact") {
     title.html('Contact');
     title.css('width', "3em");
-    $('.'+ hash).fadeOut(300).queue(populateContactView);
-    $('.work, .home').attr("display", "none");
+    $('.' + hash).animate({ opacity:0 }, 300).queue(populateContactView);
+
+    // $('.'+hash).fadeTo("slow", 0).queue(populateContactView);
+    // $('.'+ hash).fadeOut(300).queue(populateContactView);
+    $('.work, .home').css("display", "none");
   }
   window.location.hash = "contact";
   // conjures email with prepopulated fields
