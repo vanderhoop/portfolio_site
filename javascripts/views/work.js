852144c46e4b1a800f469bc82c7294ff967cf54c
Travis Vander Hoop <vanderhoop@me.com>
2014-01-05 12:43:18 -0500

    added border to all work media, corrected margin on title div, made changes to header sizes, and added insightjournal jpeg to work section

--- javascripts/views/work.js
+++ javascripts/views/work.js
@@ -13,25 +13,25 @@ var WorkView = Backbone.View.extend({
     var template = retrieveWorkTemplate();
     var projects = [
       {
-        media: '<a href="http://insight-journal.herokuapp.com/"><img class="work-img" src="images/insight-journal.jpg" alt="InsightJournal"></a>',
+        media: '<a href="http://insight-journal.herokuapp.com/"><img class="work-media" src="images/insight-journal-work.jpg" alt="InsightJournal"></a>',
         title: "InsightJournal",
         role: "Web Developer",
-        description: "The online journaling tool that grows to know you."
+        description: "The online journaling tool that grows to know you through writing patterns and sentiment analysis."
       },
       {
-        media: '<iframe src="http://player.vimeo.com/video/35587878?title=0&byline=0&portrait=0" width="360" height="203" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
+        media: '<iframe class="work-media" src="http://player.vimeo.com/video/35587878?title=0&byline=0&portrait=0" width="360" height="203" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
         title: "Granulr",
         role: "Web Developer",
         description: "A web app for breaking large goals down into infinitely smaller subgoals."
       },
       {
-        media: '<iframe src="http://player.vimeo.com/video/35587878?title=0&byline=0&portrait=0" width="360" height="203" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
+        media: '<iframe class="work-media" src="http://player.vimeo.com/video/35587878?title=0&byline=0&portrait=0" width="360" height="203" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
         title: "Ben & Jerry's - Fair Tweets",
         role: "Copywriter and VO Talent",
         description: "A fun little web app that donated unused twitter characters to spread the word about World Fair Trade Day."
       },
       {
-        media: '<iframe src="http://player.vimeo.com/video/61557103?title=0&amp;byline=0&amp;portrait=0" width="360" height="203" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
+        media: '<iframe class="work-media" src="http://player.vimeo.com/video/61557103?title=0&amp;byline=0&amp;portrait=0" width="360" height="203" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
         title: "Comedy Central - Skyfall Promos",
         role: "Video Editor",
         description: "Promos for Skyfall's release on DVD and Blu-Ray"
