7133a365eab92c28724c8df2966e69f42fecb8a2
vanderhoop <vanderhoop@me.com>
2014-08-03 18:18:05 -0400

    removes jQuery fadeOut-to-fadeIn in favor of CSS3 transition with setTimeout

--- javascripts/router.js
+++ javascripts/router.js
@@ -19,25 +19,48 @@ var Router = Backbone.Router.extend({
   home: function(){
     // for use within fadeOut callback
     var routerInstance = this;
-    this.mainContainer.fadeOut(400, function(){
+    // this.mainContainer.fadeOut(400, function(){
+    //   body.scrollTop(0);
+    //   routerInstance.homeView.render().fadeIn(600);
+    // });
+    this.mainContainer.css("opacity", 0);
+    // sets a timeout to wait for the css transition to complete before removing content
+    var timeoutID1 = setTimeout(function(){
       body.scrollTop(0);
-      routerInstance.homeView.render().fadeIn(600);
-    });
+      routerInstance.mainContainer.css("display", "none");
+      routerInstance.homeView.render().css("display", "block").css("opacity", 1.0);
+      clearTimeout(timeoutID1);
+    }, 600);
   },
 
   work: function(){
     var routerInstance = this;
-    this.mainContainer.fadeOut(400, function(){
+    // this.mainContainer.fadeOut(400, function(){
+    //   body.scrollTop(0);
+    //   routerInstance.workView.render().fadeIn(600);
+    // });
+
+    this.mainContainer.css("opacity", 0);
+    var timeoutID2 = setTimeout(function(){
       body.scrollTop(0);
-      routerInstance.workView.render().fadeIn(600);
-    });
+      routerInstance.mainContainer.css("display", "none");
+      routerInstance.workView.render().css("display", "block").css("opacity", 1.0);
+      clearTimeout(timeoutID2);
+    }, 600);
   },
 
   contact: function(){
     var routerInstance = this;
-    this.mainContainer.fadeOut(400, function(){
+    // this.mainContainer.fadeOut(400, function(){
+    //   body.scrollTop(0);
+    //   routerInstance.contactView.render().fadeIn(600);
+    // });
+    this.mainContainer.css("opacity", 0);
+    var timeoutID3 = setTimeout(function(){
       body.scrollTop(0);
-      routerInstance.contactView.render().fadeIn(600);
-    });
+      routerInstance.mainContainer.css("display", "none");
+      routerInstance.contactView.render().css("display", "block").css("opacity", 1.0);
+      clearTimeout(timeoutID3);
+    }, 600);
   }
 });
