29d3dc813a28a65c7acd72b3a00f6ca833732031
vanderhoop <vanderhoop@me.com>
2014-11-04 18:00:59 -0800

    adds battleroom info and updates metapinger copy

--- javascripts/views/work.js
+++ javascripts/views/work.js
@@ -20,13 +20,19 @@ var WorkView = Backbone.View.extend({
         media: '<a href="http://metapinger.herokuapp.com/"><div class="work-media" id="metapinger-sprite"></div></a>',
         title: "MetaPinger",
         role: "Web Developer",
-        description: "Heroku's servers are famous for idling, so I built a Heroku app that pings Heroku apps (including itself) at 10 minute intervals, ensuring perpetual wakefulness. All it took was a single dyno."
+        description: "Heroku's servers are famous for idling, so I built a Heroku app that pings Heroku apps (including itself) at 10 minute intervals, ensuring perpetual wakefulness."
       },
       {
         media: '<a href="https://vimeo.com/23211569"><div class="work-media" id="fairtweets-sprite"></div></a>',
         title: "Ben & Jerry's - Fair Tweets",
         role: "Copywriter and VO Talent",
         description: "A fun little web app that donated unused twitter characters to spread the word about World Fair Trade Day."
+      },
+      {
+        media: '<a href="https://github.com/vanderhoop/battleroom"><div class="work-media" id="battleroom-sprite"></div></a>',
+        title: "The Battleroom Gem",
+        role: "Benevolent Dictator for Life",
+        description: "A command line tool designed to give Ruby newbies countless reps doing simple tasks like working with variables, manipulating data structures, and defining and invoking methods."
       }
     ];
 
