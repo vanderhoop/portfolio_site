1760a80983276e2bf1bcb026b1f147da42e726fd
vanderhoop <vanderhoop@me.com>
2014-11-04 19:10:05 -0800

    adds battleroom image

--- javascripts/views/work.js
+++ javascripts/views/work.js
@@ -11,19 +11,19 @@ var WorkView = Backbone.View.extend({
     console.log("WorkView is initialized");
     this.projects = [
       {
-        media: '<a href="http://insightjournal.us"><div class="work-media" id="insight-journal-sprite"></div></a>',
+        media: '<a href="http://insightjournal.us"><div class="work-media" id="insight-journal-sprite"><div class="overlay"><p>+</p></div></div></a>',
         title: "InsightJournal",
         role: "Web Developer",
         description: "The private online journaling tool that grows to know you through sentiment analysis and usage patterns."
       },
       {
-        media: '<a href="http://metapinger.herokuapp.com/"><div class="work-media" id="metapinger-sprite"></div></a>',
+        media: '<a href="http://metapinger.herokuapp.com/"><div class="work-media" id="metapinger-sprite"><div class="overlay"><p>+</p></div></div></a>',
         title: "MetaPinger",
         role: "Web Developer",
         description: "Heroku's servers are famous for idling, so I built a Heroku app that pings Heroku apps (including itself) at 10 minute intervals, ensuring perpetual wakefulness."
       },
       {
-        media: '<a href="https://vimeo.com/23211569"><div class="work-media" id="fairtweets-sprite"></div></a>',
+        media: '<a href="https://vimeo.com/23211569"><div class="work-media" id="fairtweets-sprite"><div class="overlay"><p>+</p></div></div></a>',
         title: "Ben & Jerry's - Fair Tweets",
         role: "Copywriter and VO Talent",
         description: "A fun little web app that donated unused twitter characters to spread the word about World Fair Trade Day."
